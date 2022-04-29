63c48decee93e19249f78a2bbef298bb93f6bd86
Moham <Moham@DESKTOP-EKILIM5>
2022-04-29 13:50:04 +0300

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>navbar</div>
+  <slot></slot>
+  <div>footer</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <nuxt-page></nuxt-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
